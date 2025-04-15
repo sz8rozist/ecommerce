9686d33a3f3ff80d2506597f8b2c1c5ff3f3d700
Rózsa István <ristvan98@gmail.com>
2025-04-15 21:30:36 +0200

    Docker javítás

--- src/main/java/com/example/ecommerce/config/MinioConfig.java
+++ src/main/java/com/example/ecommerce/config/MinioConfig.java
@@ -1,11 +1,15 @@
 package com.example.ecommerce.config;
 
+import io.minio.BucketExistsArgs;
+import io.minio.MakeBucketArgs;
 import io.minio.MinioClient;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@Slf4j
 public class MinioConfig {
 
     @Value("${minio.endpoint}")
@@ -17,11 +21,28 @@ public class MinioConfig {
     @Value("${minio.secret-key}")
     private String secretKey;
 
+    @Value("${minio.bucket}")
+    private String bucketName;
+
     @Bean
     public MinioClient minioClient() {
-        return MinioClient.builder()
-                .endpoint(endpoint)
-                .credentials(accessKey, secretKey)
-                .build();
+        try {
+            MinioClient minioClient = MinioClient.builder()
+                    .endpoint(endpoint)
+                    .credentials(accessKey, secretKey)
+                    .build();
+
+            boolean found = minioClient.bucketExists(BucketExistsArgs.builder().bucket(bucketName).build());
+            if (!found) {
+                minioClient.makeBucket(MakeBucketArgs.builder().bucket(bucketName).build());
+                log.info("Bucket létrehozva: {}", bucketName);
+                System.out.println("Bucket létrehozva: " + bucketName);
+            } else {
+                log.info("Bucket már létezik: {}", bucketName);
+            }
+            return minioClient;
+        } catch (Exception e) {
+            throw new RuntimeException("MinIO konfiguráció közben hiba történt", e);
+        }
     }
 }
